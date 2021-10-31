2227887a622111d39af0eef0c99943757d3ed251
smahkafi <sakafi.m@gmail.com>
2021-10-31 16:57:47 +0600

    add new package add

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 const app = express();
 const cors = require('cors')
+// const ObjectId = require('mongodb').ObjectId;
 
 // config dotenv
 require('dotenv').config()
@@ -42,12 +43,29 @@ async function run() {
         res.send(result)
       })
 
-      //booking data getting 
+      //getting booking data api
       app.get('/bookings', async(req, res) =>{
         const cursor = bookingCollections.find({})
         const booking = await cursor.toArray()
         res.send(booking)
       })
+
+      // getting email api
+      app.get('/bookings/:email', async (req, res) => {
+        const email = req.params.email;
+        const result = await bookingCollections.find({email}).toArray();
+        res.json(result)
+        
+      })
+
+
+       // Add New Vacation API
+       app.post('/tours', async (req, res) => {
+        console.log(req.body);
+        const result = await toursCollections.insertOne(req.body);
+        res.send(result);
+    })
+
 
       // cancle order
       app.delete('/bookings/:id', async(req, res) => {
@@ -71,100 +89,3 @@ app.get('/', (req, res)=>{
 app.listen(port, ()=>{
   console.log('port is running on server', port);
 })
-
-
-
-
-
-// // other code 
-
-// const express = require('express');
-// const { MongoClient } = require('mongodb');
-// const ObjectId = require('mongodb').ObjectId;
-// const cors = require('cors');
-// require('dotenv').config();
-
-// const app = express();
-// const port = process.env.PORT || 5000;
-
-// // middleware
-// app.use(cors());
-// app.use(express.json());
-
-// const uri = mongodb+srv://${ process.env.DB_USER }:${ process.env.DB_PASS }@cluster0.417rb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-// async function run() {
-
-//     try {
-//         await client.connect();
-//         const database = client.db("easy_travelling");
-//         const servicesCollection = database.collection("services");
-//         const ordersCollections = database.collection("orders");
-
-
-//         // GET API
-//         app.get('/services', async (req, res) => {
-//             const cursor = servicesCollection.find({});
-//             const services = await cursor.toArray();
-//             res.send(services);
-//         });
-
-//           //POST API
-//           app.post('/orders', async (req, res) => {
-//             const orders = req.body;
-//             const result = await ordersCollections.insertOne(orders);
-//             res.json(result)
-//         });
-
-
-//         //order get
-//         app.get('/orders', async (req, res) => {
-//             const cursor = ordersCollections.find({})
-//             const orders = await cursor.toArray()
-//             res.send(orders)
-//         })
-
-       
-
-//         // // GET SINGLE SERVICE
-//         // app.get('/services/:id', async (req, res) => {
-//         //     const id = req.params.id;
-
-//         //     const query = { _id: ObjectId(id) };
-//         //     const service = await servicesCollection.findOne(query);
-//         //     res.json(service);
-//         // })
-
-       
-//         // load single service get api
-//         // app.delete('/services/:id', async (req, res) => {
-//         //     const id = req.params.id;
-//         //     const query = { _id: ObjectId(id) };
-//         //     const result = await servicesCollection.deleteOne(query);
-//         //     res.json(result);
-//         // })
-
-//           // delete service
-//           app.delete('/services/:id', async (req, res) => {
-//             const id = req.params.id;
-//             // const query = { _id: ObjectId(id) };
-//             const query = { _id: (id) };
-//             const result = await ordersCollections.deleteOne(query);
-//             res.json(result);
-//         })
-
-//     }
-//     finally {
-//         // await client.close();
-//     }
-
-// }
-// run().catch(console.dir);
-
-// app.get('/', (req, res) => {
-//     res.send('Easy Travelling');
-// });
-// app.listen(port, () => {
-//     console.log('server running at port', port);
-// });
